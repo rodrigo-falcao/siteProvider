600ec3e8f4b3dcccfef278d40a1dd390e5943275
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-10-01 18:57:43 -0300

    correção do menu mobile, scrolamento e efeito

--- src/app/components/menuMobile/MenuMobile.tsx
+++ src/app/components/menuMobile/MenuMobile.tsx
@@ -4,37 +4,56 @@ import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../../../public/images/imgLogoFull.png';
 import { FaAlignJustify, FaWindowClose } from "react-icons/fa";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSpring, animated, config } from 'react-spring';
 
 const MenuMobile = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const toggleMenu = () => setMenuOpen(!menuOpen);
-    const closeMenu = () => setMenuOpen(false);
+
+    const animationProps = useSpring({
+        opacity: menuOpen ? 1 : 0,
+        transform: menuOpen ? 'translateX(0%)' : 'translateX(-100%)',
+        config: { ...config.gentle, friction: 20 }
+    });
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.style.overflow = 'hidden'; // Desativa a rolagem quando o menu estiver aberto
+        } else {
+            document.body.style.overflow = 'auto';   // Reativa a rolagem quando o menu estiver fechado
+        }
+    
+        // Limpeza ao desmontar o componente
+        return () => {
+            document.body.style.overflow = 'auto'; 
+        };
+    }, [menuOpen]);
 
     return (
         <header className={styles.header}>
             <div className={styles.menu}>
                 <FaAlignJustify onClick={toggleMenu} size={24} className={styles.mobileSvg} />
-                    <Image 
-                        src={logo}
-                        alt='logo'
-                        className={styles.mobileImg}
-                    />
+                <Image 
+                    src={logo}
+                    alt='logo'
+                    className={styles.mobileImg}
+                />
             </div>
 
             {menuOpen && (
-                    <nav className={styles.menuList}>
-                            {menuOpen && (
-                                <FaWindowClose onClick={closeMenu} className={styles.buttonCloseMenuMobile}/>
-                            )}
-                            <ul>
-                                <li><Link href="/#Menu" onClick={closeMenu}>HOME</Link></li>
-                                <li><Link href="/#Presentation" onClick={closeMenu}>SERVICES</Link></li>
-                                <li><Link href="/#Tecnologies" onClick={closeMenu}>WORK</Link></li>
-                                <li><Link href="/#Insights" onClick={closeMenu}>INSIGHTS</Link></li>
-                                <li><Link href="/#Infos" onClick={closeMenu}>COMPANY</Link></li>
-                            </ul>
-                    </nav>
+                <animated.nav style={animationProps} className={styles.menuList}>
+                    {menuOpen && (
+                        <FaWindowClose onClick={toggleMenu} className={styles.buttonCloseMenuMobile}/>
+                    )}
+                    <ul>
+                        <li><Link href="/#Menu" onClick={toggleMenu}>HOME</Link></li>
+                        <li><Link href="/#Presentation" onClick={toggleMenu}>SERVICES</Link></li>
+                        <li><Link href="/#Tecnologies" onClick={toggleMenu}>WORK</Link></li>
+                        <li><Link href="/#Insights" onClick={toggleMenu}>INSIGHTS</Link></li>
+                        <li><Link href="/#Infos" onClick={toggleMenu}>COMPANY</Link></li>
+                    </ul>
+                </animated.nav>
             )}
         </header>
     );
